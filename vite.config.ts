f9fc815ec0b469f9bfa05c2185b55b43c62f04c5
yanhhhhhh <1801084586@qq.com>
2023-10-20 00:43:28 +0800

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 const timestamp = new Date().getTime();
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/demo/',
   plugins: [react()],
 
   resolve: {
