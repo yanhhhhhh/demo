bee7d9ae81ea7a0b5d25f8241339c97f33c4d229
yanhhhhhh <1801084586@qq.com>
2023-10-20 01:00:19 +0800

    basename

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -14,48 +14,43 @@ import App from '@/App';
 import Test from '@/views/test';
 import Apply from '@/views/product/intro/apply-scene';
 
-const router = createHashRouter(
-  [
-    {
-      path: '/',
-      element: <Navigate to="/product_intro" />,
-    },
-    {
-      path: '/',
-      element: <App />,
-      children: [
-        {
-          path: '/product_intro',
-          element: <ProductIntro />,
-        },
-        {
-          path: '/product_detail',
-          element: <ProductDetail />,
-        },
-        {
-          path: '/services_subscriptions',
-          element: <ServicesSubscriptions />,
-        },
-        {
-          path: '/test',
-          element: <Test />,
-        },
-        {
-          path: '/apply',
-          element: <Apply />,
-        },
-      ],
-      // errorElement
-    },
-
-    {
-      path: '*',
-      element: <NoMatch />,
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: '/demo',
-  }
-);
+    path: '/',
+    element: <Navigate to="/product_intro" />,
+  },
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/product_intro',
+        element: <ProductIntro />,
+      },
+      {
+        path: '/product_detail',
+        element: <ProductDetail />,
+      },
+      {
+        path: '/services_subscriptions',
+        element: <ServicesSubscriptions />,
+      },
+      {
+        path: '/test',
+        element: <Test />,
+      },
+      {
+        path: '/apply',
+        element: <Apply />,
+      },
+    ],
+    // errorElement
+  },
+
+  {
+    path: '*',
+    element: <NoMatch />,
+  },
+]);
 
 export default router;
